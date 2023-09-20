25ee866159b7a15bdeedb8e042f03828922fcbdc
mfewton3 <mfewton3@gmail.com>
2023-09-19 19:27:59 -0500

    commit

---
+++ hardhat.config-goerli2.js
@@ -0,0 +1,17 @@
+require("dotenv").config()
+require("@nomicfoundation/hardhat-toolbox")
+
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    localhost: {},
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts:privateKeys.split(','),
+
+    },    
+  },
+};
