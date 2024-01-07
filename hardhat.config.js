3590764fe59098fbf70e7fe8f5d980ac03bebbec
mfewton3 <mfewton3@gmail.com>
2024-01-07 10:50:45 -0600

    before Seploia Testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ module.exports = {
     hardhat: {
       forking: {
         url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-        blockNumber: 18656870
+        blockNumber: 18956198
       }
     }
   }
